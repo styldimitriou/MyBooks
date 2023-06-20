13d70150279e39fe7dc3b68d9fa8d6665651639d
Stelios Dimitriou <s.dimitriou@thebeat.co>
2023-06-20 21:52:23 +0300

    Implement fetch book list from Gutendex API

--- MyBooks/ViewModels/BookViewModel.swift
+++ MyBooks/ViewModels/BookViewModel.swift
@@ -11,19 +11,45 @@ import Combine
 class BookViewModel: ObservableObject {
     @Published var books: [Book] = []
     @Published var favoriteBooks: [Book] = []
+    
+    private var subscribers: Set<AnyCancellable> = []
         
     init() {
         fetchBooks()
     }
     
     func fetchBooks() {
-        let mockBooks = [
-            Book(id: 1, title: "Book 1", author: "Author 1", coverImageURL: URL(string: "https://example.com/cover1.jpg")!, languages: [], downloadCount: 1),
-            Book(id: 2, title: "Book 2", author: "Author 2", coverImageURL: URL(string: "https://example.com/cover2.jpg")!, languages: [], downloadCount: 1),
-            Book(id: 3, title: "Book 3", author: "Author 3", coverImageURL: URL(string: "https://example.com/cover3.jpg")!, languages: [], downloadCount: 1)
-        ]
+        guard let url = URL(string: "https://gutendex.com/books") else {
+            return
+        }
         
-        books = mockBooks
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { response -> Data in
+                guard
+                    let httpURLResponse = response.response as? HTTPURLResponse,
+                    httpURLResponse.statusCode == 200
+                else {
+                    throw BookError.statusCode
+                }
+                return response.data
+            }
+            .decode(type: BookListRaw.self, decoder: JSONDecoder())
+            .map { bookList -> [Book] in
+                bookList.results.map({ Book(id: $0.id,
+                                            title: $0.title,
+                                            author: $0.authors.map({$0.name}).joined(separator: ","),
+                                            coverImageURL: $0.formats.imageURL,
+                                            languages: $0.languages,
+                                            downloadCount: $0.downloadCount)})
+            }
+            .mapError {
+                return BookError.map($0)
+            }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }) { [weak self] books in
+                self?.books = books
+            }
+            .store(in: &subscribers)
     }
     
     func toggleFavorite(book: Book) {
