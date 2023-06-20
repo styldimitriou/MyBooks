3ee0cf51f9536b86338c868b11c01e7966375c31
Stelios Dimitriou <s.dimitriou@thebeat.co>
2023-06-20 16:39:20 +0300

    Create Book data model

---
+++ MyBooks/Models/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  MyBooks
+//
+//  Created by Stylianos Dimitriou on 20/6/23.
+//
+
+import Foundation
+
+struct Book: Identifiable, Decodable {
+    let id: Int
+    let title: String
+    let author: String
+    let coverImageURL: URL?
+    var isFavorite: Bool = false
+    let languages: [String]
+    let downloadCount: Int
+}
