3daa2c7dc139cb4aed663d3ea85d3cdb0ff6f5eb
Stelios Dimitriou <s.dimitriou@thebeat.co>
2023-06-20 14:37:42 +0300

    Create new project

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Stylianos Dimitriou on 20/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
