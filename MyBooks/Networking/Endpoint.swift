82c1a98b57bba96e261da5fb57bdc0b2f356b608
Stelios Dimitriou <s.dimitriou@thebeat.co>
2023-06-20 23:37:07 +0300

    Create Endpoint protocol with default implementation

---
+++ MyBooks/Networking/Endpoint.swift
@@ -0,0 +1,27 @@
+//
+//  Endpoint.swift
+//  MyBooks
+//
+//  Created by Stylianos Dimitriou on 20/6/23.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var baseURL: String { get }
+    var path: String {get}
+    var parameter: [URLQueryItem] { get }
+    
+    func getURL() -> URL?
+}
+
+extension Endpoint {
+    func getURL() -> URL? {
+        var component = URLComponents()
+        component.scheme = "https"
+        component.host = baseURL
+        component.path = path
+        component.queryItems = parameter
+        return component.url
+    }
+}
